b66e47fc8cf3ee1e01da4fdb8197968aa81b910c
JCis <jcismx@gmail.com>
2022-09-24 02:38:32 -0500

    2dn version

--- CoupaMX/src/com/main/CoupaIntegrations.java
+++ CoupaMX/src/com/main/CoupaIntegrations.java
@@ -137,20 +137,3 @@ public class CoupaIntegrations {
 
 }
 
-//import java.util.ArrayList;
-//public class ListToArray {
-// public static void main(String args[]){
-//    ArrayList<String> list = new ArrayList<String>();
-//    list.add("Apple");
-//    list.add("Orange");
-//    list.add("Banana");
-//
-//    System.out.println("Contents of list ::"+list);
-//    String[] myArray = new String[list.size()];
-//    list.toArray(myArray);
-//
-//    for(int i=0; i<myArray.length; i++){
-//       System.out.println("Element at the index "+i+" is ::"+myArray[i]);
-//    }
-// }
-//}
